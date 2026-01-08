5383e1034f1de87a7c96cc5525342d432feab154
Lealene Abrigo <lealene.abrigo@cvsu.edu.ph>
2026-01-08 22:16:25 +0800

    p6
    p6

--- objs/phy_exp_1.gd
+++ objs/phy_exp_1.gd
@@ -12,7 +12,7 @@ func _process(_delta: float) -> void:
 
 
 func _on_back_pressed() -> void:
-	get_tree().change_scene_to_file("res://objs/physics.tscn")
+	get_tree().change_scene_to_file("res://physics.tscn")
 
 
 func _on_start_pressed() -> void:
